467650ae690f89ae8043e97eb633df4cbcb7ece9
Ryan Remer <remerrd@byu.edu>
2017-11-27 14:30:18 -0700

    simple array

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,4 +4,9 @@ app.controller('mainCtrl',mainCtrl);
 
 function mainCtrl($scope, $http){
     $scope.test = "HELLO WORLD!";
+
+    $scope.persons = [];
+    $scope.persons.push({name:"Fred", votes:0});
+    $scope.persons.push({name:"Bob", votes:2});
+    $scope.persons.push({name:"Me", votes:1});
 }
