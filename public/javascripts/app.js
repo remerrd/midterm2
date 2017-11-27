512f01daec4722e764b67a7e108deef8cc087f83
Ryan Remer <remerrd@byu.edu>
2017-11-27 14:34:22 -0700

    add to Array

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,4 +9,11 @@ function mainCtrl($scope, $http){
     $scope.persons.push({name:"Fred", votes:0});
     $scope.persons.push({name:"Bob", votes:2});
     $scope.persons.push({name:"Me", votes:1});
+
+    $scope.addPerson = function(){
+        $scope.persons.push({
+            name: $scope.name,
+            votes: 0
+        });
+    }
 }
